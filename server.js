cae1a47baa14042b307f7a5e9fe47db564b9c074
A W <amwtablet@gmail.com>
2022-02-12 16:41:18 -0500

    added errors and dbCongig for Postgres attachment

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
+const { pool } = require("./dbConfig");
 
 const PORT = process.env.PORT || 4000;
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: false}));
 
 
 app.get("/", (req, res) => {
@@ -21,6 +24,37 @@ app.get('/users/login', (req, res) => {
 app.get('/users/dashboard', (req, res) => {
     res.render("dashboard", { user: "andy" });
 });
+
+app.post('/users/register', (req, res) => {
+    let { name, email, password, password2 } = req.body;
+
+    console.log({
+        name,
+        email,
+        password,
+        password2
+    });
+
+    let errors = [];
+
+    if  (!name || !email || !password || !password2) {
+        errors.push({ message: "All Fields Required" });
+    }
+
+    if(password.length < 6) {
+        errors.push({ message: "Password Minimum: 6 Characters" });
+    }
+
+    if(password != password2) {
+        errors.push({ message: "Passwords Do Not Match" });
+    }
+
+    if(errors.length > 0) {
+        res.render('register', { errors });
+    }
+});
+
+
 
 
 
