29ce85bee6047d39ee25a80c4a3d7bcdc8cb19b5
A W <amwtablet@gmail.com>
2022-02-13 09:55:32 -0500

    added password hashing with bcrypt, page re-generation on error, and passportConfig file

--- server.js
+++ server.js
@@ -1,32 +1,61 @@
 const express = require("express");
-const app = express();
 const { pool } = require("./dbConfig");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+require("dontenv").config();
+const app = express();
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
-app.set("view engine", "ejs");
+const initializePassport = require("./passportConfig");
+
+initializePassport(passport);
 
 app.use(express.urlencoded({ extended: false}));
+app.set("view engine", "ejs");
+
+app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false
+    })
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(flash());
 
 
 app.get("/", (req, res) => {
     res.render('index');
 });
 
-app.get('/users/register', (req, res) => {
-    res.render("register");
+app.get('/users/register', checkAuthenticated, (req, res) => {
+    res.render("register.ejs");
 });
 
-app.get('/users/login', (req, res) => {
-    res.render("login");
+app.get('/users/login', checkAuthenticated, (req, res) => {
+    console.log(req.session.flash.error);
+    res.render("login.ejs");
 });
 
-app.get('/users/dashboard', (req, res) => {
-    res.render("dashboard", { user: "andy" });
+app.get('/users/dashboard', checkNotAuthenticated, (req, res) => {
+    console.log(req.isAuthenticated());
+    res.render("dashboard", { user: req.user.name });
 });
 
-app.post('/users/register', (req, res) => {
+app.get("/users/logout", (req, res) => {
+    req.logout();
+    res.render("index", { message: "You have logged out successfully" });
+  });
+
+app.post('/users/register', async (req, res) => {
     let { name, email, password, password2 } = req.body;
+
+    let errors = [];
 
     console.log({
         name,
@@ -34,8 +63,6 @@ app.post('/users/register', (req, res) => {
         password,
         password2
     });
-
-    let errors = [];
 
     if  (!name || !email || !password || !password2) {
         errors.push({ message: "All Fields Required" });
@@ -50,15 +77,73 @@ app.post('/users/register', (req, res) => {
     }
 
     if(errors.length > 0) {
-        res.render('register', { errors });
+        //re-renders page if there are errors
+        res.render("register", { errors, name, email, password, password2 });
+    } else {
+      hashedPassword = await bcrypt.hash(password, 10);
+      console.log(hashedPassword);
+      // Validation passed
+      pool.query(
+        `SELECT * FROM users
+          WHERE email = $1`,
+        [email],
+        (err, results) => {
+          if (err) {
+            console.log(err);
+          }
+          console.log(results.rows);
+  
+          if (results.rows.length > 0) {
+            return res.render("register", {
+              message: "Email already registered"
+            });
+          } else {
+            pool.query(
+              `INSERT INTO users (name, email, password)
+                  VALUES ($1, $2, $3)
+                  RETURNING id, password`,
+              [name, email, hashedPassword],
+              (err, results) => {
+                if (err) {
+                  throw err;
+                }
+                console.log(results.rows);
+                req.flash("success_msg", "You are now registered. Please log in");
+                res.redirect("/users/login");
+              }
+            );
+          }
+        }
+      );
     }
 });
 
 
-
-
-
-app.listen(PORT, () => {
+app.post(
+    "/users/login",
+    passport.authenticate("local", {
+      successRedirect: "/users/dashboard",
+      failureRedirect: "/users/login",
+      failureFlash: true
+    })
+  );
+  
+  function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return res.redirect("/users/dashboard");
+    }
+    next();
+  }
+  
+  function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect("/users/login");
+  }
+  
+  app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-});
+  });
+  
 
